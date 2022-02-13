8f57f8a8d8023eb1c28a680242233637f07a4a64
JigSaw16 <96497335+JigSaw16@users.noreply.github.com>
2022-02-13 20:37:41 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,160 +3,16 @@ import random
 import numpy as np
 import cv2
 from sklearn.ensemble import RandomForestClassifier
-import pandas
+import pandas as pd
 from PIL import Image
 from collections import namedtuple
 import xml.etree.ElementTree as ET
 from os import listdir
 from os.path import isfile, join
+import time
+from sklearn.metrics import confusion_matrix
 
 
-
-class_id_to_new_class_id = {0: 0,
-                            1: 0,
-                            2: 0,
-                            3: 0,
-                            4: 0,
-                            5: 0,
-                            6: -1,
-                            7: 0,
-                            8: 0,
-                            9: 0,
-                            10: 0,
-                            11: 1,
-                            12: -1,
-                            13: 1,
-                            14: 0,
-                            15: 0,
-                            16: 0,
-                            17: 0,
-                            18: 1,
-                            19: 1,
-                            20: 1,
-                            21: 1,
-                            22: 1,
-                            23: 1,
-                            24: 1,
-                            25: 1,
-                            26: 1,
-                            27: 1,
-                            28: 1,
-                            29: 1,
-                            30: 1,
-                            31: 1,
-                            32: -1,
-                            33: 2,
-                            34: 2,
-                            35: 2,
-                            36: 2,
-                            37: 2,
-                            38: 2,
-                            39: 2,
-                            40: 2,
-                            41: -1,
-                            42: -1}
-
-
-def load_data(path, filename):
-    """
-    Loads data from disk.
-    @param path: Path to dataset directory.
-    @param filename: Filename of csv file with information about samples.
-    @return: List of dictionaries, one for every sample, with entries "image" (np.array with image) and "label" (class_id).
-    """
-    entry_list = pandas.read_csv(os.path.join(path, filename))
-
-    data = []
-    for idx, entry in entry_list.iterrows():
-        class_id = class_id_to_new_class_id[entry['ClassId']]
-        image_path = entry['Path']
-
-        if class_id != -1:
-            image = cv2.imread(os.path.join(path, image_path))
-            data.append({'image': image, 'label': class_id})
-
-    return data
-
-
-def draw_grid(images, n_classes, grid_size, h, w):
-    """
-    Draws images on a grid, with columns corresponding to classes.
-    @param images: Dictionary with images in a form of (class_id, list of np.array images).
-    @param n_classes: Number of classes.
-    @param grid_size: Number of samples per class.
-    @param h: Height in pixels.
-    @param w: Width in pixels.
-    @return: Rendered image
-    """
-    image_all = np.zeros((h, w, 3), dtype=np.uint8)
-    h_size = int(h / grid_size)
-    w_size = int(w / n_classes)
-
-    col = 0
-    for class_id, class_images in images.items():
-        for idx, cur_image in enumerate(class_images):
-            row = idx
-
-            if col < n_classes and row < grid_size:
-                image_resized = cv2.resize(cur_image, (w_size, h_size))
-                image_all[row * h_size: (row + 1) * h_size, col * w_size: (col + 1) * w_size, :] = image_resized
-
-        col += 1
-
-    return image_all
-
-
-def display(data):
-    """
-    Displays samples of correct and incorrect classification.
-    @param data: List of dictionaries, one for every sample, with entries "image" (np.array with image), "label" (class_id),
-                    "desc" (np.array with descriptor), and "label_pred".
-    @return: Nothing.
-    """
-    n_classes = 3
-
-    corr = {}
-    incorr = {}
-
-    for idx, sample in enumerate(data):
-        if sample['desc'] is not None:
-            if sample['label_pred'] == sample['label']:
-                if sample['label_pred'] not in corr:
-                    corr[sample['label_pred']] = []
-                corr[sample['label_pred']].append(idx)
-            else:
-                if sample['label_pred'] not in incorr:
-                    incorr[sample['label_pred']] = []
-                incorr[sample['label_pred']].append(idx)
-
-            # print('ground truth = %s, predicted = %s' % (sample['label'], pred))
-            # cv2.imshow('image', sample['image'])
-            # cv2.waitKey()
-
-    grid_size = 8
-
-    # sort according to classes
-    corr = dict(sorted(corr.items(), key=lambda item: item[0]))
-    corr_disp = {}
-    for key, samples in corr.items():
-        idxs = random.sample(samples, min(grid_size, len(samples)))
-        corr_disp[key] = [data[idx]['image'] for idx in idxs]
-    # sort according to classes
-    incorr = dict(sorted(incorr.items(), key=lambda item: item[0]))
-    incorr_disp = {}
-    for key, samples in incorr.items():
-        idxs = random.sample(samples, min(grid_size, len(samples)))
-        incorr_disp[key] = [data[idx]['image'] for idx in idxs]
-
-    image_corr = draw_grid(corr_disp, n_classes, grid_size, 800, 600)
-    image_incorr = draw_grid(incorr_disp, n_classes, grid_size, 800, 600)
-
-    cv2.imshow('images correct', image_corr)
-    cv2.imshow('images incorrect', image_incorr)
-    cv2.waitKey()
-
-    # this function does not return anything
-    return
 
 
 def bb_intersection_over_union(boxA, boxB):
@@ -175,31 +31,60 @@ def bb_intersection_over_union(boxA, boxB):
     return iou
 
 
+def learn_bovw(data):
+    dict_size = 92
+    bow = cv2.BOWKMeansTrainer(dict_size)
+    sift = cv2.SIFT_create()
+    ctrr = 1
 
-def main():
+    for sample in data:
+        img = cv2.imread('../train/images/' + sample.get("image_name"))
+        sample_m = cv2.cvtColor(img, None)
 
-    allfiles = [f for f in listdir("../train/annotations") if isfile(join("../train/annotations", f))]
-    speedlimit = []
-    other = []
-    tree = []
-    all_data = []
+        k_pts = sift.detect(sample['image_name'], None)
+        k_pts, desc = sift.compute(sample_m, k_pts)
 
-    for file in range(len(allfiles)):
-        pth = '../train/annotations/road' + str(file) + '.xml'
-        tree.append(ET.parse(pth))
+        if desc is not None:
+            bow.add(desc)
 
-    Detection = namedtuple("Detection", ["image_name", "numb_of_obj", "box_coords"])
+        print('Progress: ', ctrr, '/', len(data))
+        ctrr += 1
 
-    print(len(allfiles))
+    vocabulary = bow.cluster()
+    np.save('voc.npy', vocabulary)
+
+
+def extract_features(data, path):
+    sift = cv2.SIFT_create()
+    flann = cv2.FlannBasedMatcher_create()
+    bow = cv2.BOWImgDescriptorExtractor(sift, flann)
+    vocabulary = np.load('voc.npy')
+    bow.setVocabulary(vocabulary)
+
+    for sample in data:
+        img = cv2.imread(path + sample["image_name"])
+        sample_m = cv2.cvtColor(img, None)
+        k_pts = sift.detect(sample_m, None)
+        img_des = bow.compute(sample_m, k_pts)
+        if img_des is not None:
+            sample.update({'desc': img_des})
+        else:
+            sample.update({'desc': np.zeros((1, 92))})
+    return data
+
+
+def build_data(_all_data_, allfiles, tree):
     for n_file in range(len(allfiles)):
+        collect_data = {"image_name": None, "numb_of_obj": None, "box_coords": None, "type": None}
         root = tree[n_file].getroot()
-        print(root[1].text)
+        collect_data.update({"image_name": root[1].text})
+
         ctr = 0
-        box = []
         boxes = []
+        types = []
         for child in root.findall('object'):
             ctr += 1
-        print(ctr)
+        collect_data.update({"numb_of_obj": ctr})
 
         for child2 in root.findall('object'):
             for child3 in child2.findall('bndbox'):
@@ -209,44 +94,164 @@ def main():
                 ymax = int(child3[3].text)
                 box = [xmin, ymin, xmax, ymax]
                 boxes.append(box)
-                print(box[0], box[1], box[2], box[3])
-        print('\n')
-        all_data.append(Detection(root[1].text, ctr, boxes))
+                collect_data.update({"box_coords": boxes})
 
-    for detection in all_data:
-        # load the image
-        image_path = '../train/images/' + detection.image_name
+            for child4 in child2.findall('name'):
+                if child4.text == "speedlimit":
+                    types.append(child4.text)
+                    collect_data.update({'label': 1})
+                else:
+                    types.append("Other")
+                    collect_data.update({'label': 0})
+                collect_data.update({"type": types})
+        _all_data_.append(collect_data)
+    return _all_data_
+
+
+def train(data):
+    clf = RandomForestClassifier(92)
+    x_matrix = np.empty((1, 92))
+    y_vector = []
+    for sample in data:
+        y_vector.append(sample['label'])
+        x_matrix = np.vstack((x_matrix, sample['desc']))
+    clf.fit(x_matrix[1:], y_vector)
+
+    return clf
+
+
+def predict(rf, data):
+    for sample in data:
+        sample.update({'label_pred': rf.predict(sample['desc'])[0]})
+
+    return data
+
+
+def input_data(data_test):
+    print("Enter the command (classify/detect):")
+    user_input = input()
+    if user_input == "classify":
+        print("Enter image name:")
+        input_name = input()
+        for sample in data_test:
+            if input_name == sample["image_name"]:
+                print(sample["type"])
+
+    elif user_input == "detect":
+        for x in range(len(data_test)):
+            print(data_test[x].get("image_name"), '\n',
+                  data_test[x].get("numb_of_obj"))
+            for cords in range(len(data_test[x].get("box_coords"))):
+                print(data_test[x].get("box_coords")[cords])
+            print('')
+
+
+def create_rectangles(_all_data_train, _all_data_test, path):
+    img_with_boxes = []
+    for curr_data in range(len(_all_data_train)):
+        image_path = path + _all_data_train[curr_data].get("image_name")
         image_plus_box = cv2.imread(image_path)
 
-        # draw the ground-truth bounding box along with the predicted
-        # bounding box
-        for sing_box in range(len(detection.box_coords)):
-            cv2.rectangle(image_plus_box, tuple(detection.box_coords[sing_box][:2]), tuple(detection.box_coords[sing_box][2:]), (0, 255, 0), 2)
-        # cv2.rectangle(image, tuple(detection.pred[:2]), tuple(detection.pred[2:]), (0, 0, 255), 2)
+        for sing_box in range(len((_all_data_train[curr_data].get("box_coords")))):
+            cv2.rectangle(image_plus_box, _all_data_train[curr_data].get("box_coords")[sing_box][:2], _all_data_train[curr_data].get("box_coords")[sing_box][2:], (0, 255, 0), 2)
+            # cv2.rectangle(image_plus_box, _all_data_test[curr_data].get("box_coords")[sing_box][:2], _all_data_test[curr_data].get("box_coords")[sing_box][2:], (0, 0, 255), 2)
 
-        # compute the intersection over union and display it
         # iou = bb_intersection_over_union(detection.gt, detection.pred)
         # cv2.putText(image, "IoU: {:.4f}".format(iou), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
         # print("{}: {:.4f}".format(image_path, iou))
+        img_with_boxes.append(image_plus_box)
+    return img_with_boxes
 
-        # show the output image
-        cv2.imshow("Image", image_plus_box)
-        cv2.waitKey(0)
+def set_data():
+    allfiles_train = [f1 for f1 in listdir("../train/annotations") if isfile(join("../train/annotations", f1))]
+    allfiles_test = [f2 for f2 in listdir("../test/annotations") if isfile(join("../test/annotations", f2))]
+    tree1 = []
+    tree2 = []
+    for file1 in range(len(allfiles_train)):
+        pth1 = '../train/annotations/road' + str(file1) + '.xml'
+        tree1.append(ET.parse(pth1))
+
+    for file2 in range(len(allfiles_test)):
+        pth2 = '../test/annotations/road' + str(file2) + '.xml'
+        tree2.append(ET.parse(pth2))
+
+    train_path = '../train/images/'
+    test_path = '../test/images/'
+
+    return allfiles_train, allfiles_test, tree1, tree2,  train_path, test_path
+
+
+def display(data):
+    n_classes = 3
+
+    corr = {}
+    incorr = {}
+
+    for idx, sample in enumerate(data):
+        if sample['desc'] is not None:
+            if sample['label_pred'] == sample['label']:
+                if sample['label_pred'] not in corr:
+                    corr[sample['label_pred']] = []
+                corr[sample['label_pred']].append(idx)
+            else:
+                if sample['label_pred'] not in incorr:
+                    incorr[sample['label_pred']] = []
+                incorr[sample['label_pred']].append(idx)
+
+    print("Number of correct images:", len(corr[0]))
+    print("Number of incorrect images:", len(incorr[1]))
+    grid_size = 8
+
+    corr = dict(sorted(corr.items(), key=lambda item: item[0]))
+    corr_disp = {}
+    for key, samples in corr.items():
+        idxs = random.sample(samples, grid_size)
+        corr_disp[key] = [data[idx]['image_name'] for idx in idxs]
+    # sort according to classes
+    incorr = dict(sorted(incorr.items(), key=lambda item: item[0]))
+    incorr_disp = {}
+    for key, samples in incorr.items():
+        idxs = random.sample(samples, grid_size)
+        incorr_disp[key] = [data[idx]['image_name'] for idx in idxs]
+    return
+
+
+def main():
+
+    start = time.time()
+    all_data_train = []
+    all_data_test = []
+    allfiles_train, allfiles_test, tree1, tree2, train_path, test_path = set_data()
+
+    print('Load data')
+    build_data(all_data_train, allfiles_train, tree1)
+    build_data(all_data_test, allfiles_test, tree2)
 
 
 
+    print('learning BoVW')
+    if os.path.isfile('voc.npy'):
+        print('BoVW is already learned')
+    else:
+        learn_bovw(all_data_train)
 
-    # img = cv2.imread('../train/images/road0.png')
+    print('extracting train features')
+    all_data_train = extract_features(all_data_train, train_path)
 
-    # col = cv2.cvtColor(img, None)
-    # sift = cv2.SIFT_create()
-    # kp = sift.detect(col, None)
-    # img = cv2.drawKeypoints(col, kp, img)
-    #
-    # cv2.imwrite('sift_keypoints.png', img)
-    # cv2.imshow('test image', img)
-    # cv2.waitKey()
-    # print("Hello!")
+    print('training')
+    rf = train(all_data_train)
+
+    print('extracting test features')
+    all_data_test = extract_features(all_data_test, test_path)
+
+    print('testing')
+    all_data_test = predict(rf, all_data_test)
+
+    display(all_data_test)
+
+    input_data(all_data_test)
+    end = time.time()
+    print(end - start)
 
     return
 
